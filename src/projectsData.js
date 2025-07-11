1f0c52ec1cf6e7ec7e4223958d7f2df0b2ee31ca
Paras Jagdale <parasjagdale15@gmail.com>
2025-07-12 00:15:55 +0530

    first commit

---
+++ src/projectsData.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: "MERN-Based Investment Platform (Inspired by Zerodha)",
+    description: "A full-featured stock trading interface using React, Node.js, and MongoDB.",
+    techStack: ["React", "Node.js", "MongoDB"],
+    liveLink: "https://zerodha-clone-frontend.vercel.app/",
+    codeLink: "https://github.com/ParasBot/Zerodha-Clone",
+    image: "/images/mern.png",
+  },
+  {
+    title: "Django-Based Image Sharing App (Shario)",
+    description: "A platform to upload and share images securely, built with Django and Bootstrap.",
+    techStack: ["Django", "Bootstrap", "SQLite"],
+    liveLink: "https://github.com/ParasBot/Django-intership-project",
+    codeLink: "https://github.com/ParasBot/Django-intership-project",
+    image: "/images/python.png",
+  },
+  {
+    title: "Weather Forecast React App",
+    description: "A responsive weather app fetching real-time weather data via OpenWeather API.",
+    techStack: ["React", "API", "CSS"],
+    liveLink: "https://weather-app-react-three-nu.vercel.app/",
+    codeLink: "https://github.com/ParasBot/Weather-App-React",
+    image: "/images/weather.png",
+  },
+];
